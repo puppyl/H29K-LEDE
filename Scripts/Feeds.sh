3b25a83e1f85c278ee804a8a7a5740e93fc6e3d6
puppyl <30889203+puppyl@users.noreply.github.com>
2026-02-26 17:18:46 +0800

    Update Feeds.sh

--- Scripts/Feeds.sh
+++ Scripts/Feeds.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
